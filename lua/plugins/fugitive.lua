a4000e597686bf73220aba12b1971ea7715f55c6
Chance093 <chance.dev093@gmail.com>
2024-08-14 21:21:04 -0700

    add gitsigns plugin

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,9 +1,19 @@
 return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
-    vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
-    vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
-  end
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+
+      vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+      vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
+      vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
+      vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+    end,
+  },
 }
